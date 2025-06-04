84d06317a67313a9081e1aad4fa22018243a934f
nayunoh <brilliantjay@naver.com>
2025-06-04 11:21:57 +0900

    new plugins

---
+++ plugins/select_fruits.sh
@@ -0,0 +1,11 @@
+FRUIT=$1
+if [ $FRUIT == APPLE ]; then
+	echo "You selected Apple!"
+elif [ $FRUIT == ORANGE ]; then
+	echo "You selected Orange!"
+elif [ $FRUIT == GRAPHE ]; then
+	echo "You selected Graphe!"
+else
+	echo "You selected other Fruit!"
+
+fi	
