0fe0d6e0431d80ca6624e5b1481f8ef2d6a6edd6
nayunoh <brilliantjay@naver.com>
2025-06-04 17:21:23 +0900

    bash_xcom

---
+++ dags/dags_bash_with_xcom.py
@@ -0,0 +1,33 @@
+import pendulum
+# Airflow 3.0 부터 아래 경로로 import 합니다.
+#from airflow.providers.standard.operators.bash import BashOperator
+#from airflow.sdk import DAG
+
+# Airflow 2.10.5 이하 버전에서 실습시 아래 경로에서 import 하세요.
+from airflow.operators.bash import BashOperator
+from airflow import DAG
+
+with DAG(
+    dag_id="dags_bash_with_xcom",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    bash_push = BashOperator(
+        task_id = 'bash_push',
+        bash_commands = "echo START && "
+                        "echo XCOM_PUSHED "
+                        "{{ ti.xcom_push(key='bash_pushed, value='first_bash_message') }} && "
+                        "echo COMPLETE"
+    )
+
+    bash_pull = BashOperator(
+        task_id='bash_pull',
+        env ={'PUSHED_VALUE':"{{ ti.xcom_pull(key='bash_pushed') }}",
+              'RETURN_VALUE': "{{ ti.xcom_pull(task_ids='bash_push')}}"},
+
+        bash_command="echo $PUSHED_VALUE && echo $RETURN_VALUE",
+        do_xcom_push = False              
+    )
+
+    bash_push >> bash_pull
