5baf5df38f6cf8c73cbfb15df2ea9fd9dcd80b54
nayunoh <brilliantjay@naver.com>
2025-06-05 09:25:23 +0900

    modified

--- dags/dags_python_bash_xcom.py
+++ dags/dags_python_bash_xcom.py
@@ -9,7 +9,7 @@ from airflow import DAG
 from airflow.decorators  import task
 
 with DAG(
-    dag_id="dags_bash_with_xcom",
+    dag_id="dags_python_bash_xcom",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
