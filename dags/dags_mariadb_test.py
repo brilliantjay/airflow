e84391806fb8878831d53702ea3ec6c5a8a6ef35
nayunoh <brilliantjay@naver.com>
2025-06-05 14:07:14 +0900

    modified

--- dags/dags_mariadb_test.py
+++ dags/dags_mariadb_test.py
@@ -15,7 +15,7 @@ with DAG(
         task_id="create_country_table",
         conn_id="test",
         sql=r"""
-        CREATE TABLE rdb.Country5 (
+        CREATE TABLE rdb.Country6 (
             country_id INT NOT NULL AUTO_INCREMENT,
             name VARCHAR(50),
             continent VARCHAR(50),
@@ -28,20 +28,19 @@ with DAG(
         task_id="populate_user_table",
         conn_id="test",
         sql=r"""
-                INSERT INTO rdb.rdb_user (USER_NAME, USER_ID, USER_ROLE, USER_PW,IS_ADMIN) VALUES ('test3', 'test1234567', 'ADMIN', 'welco123!','Y');               
+                INSERT INTO rdb.rdb_user (USER_NAME, USER_ID, USER_ROLE, USER_PW,IS_ADMIN) VALUES ('test3', 'test1234568', 'ADMIN', 'welco123!','Y');               
                 """,
     )
     get_rdb_user = SQLExecuteQueryOperator(
         task_id="get_rdb.rdb_user",
         conn_id="test",
-        sql=r"""SELECT * FROM rdb.rdb_user;""",
-        handler=lambda x: print(x) 
+        sql=r"""SELECT USER_ID, USER_PW, USER_NAME, USER_ROLE, IS_ADMIN FROM rdb.rdb_user;"""    
     )
    
     get_rdb_one_user = SQLExecuteQueryOperator(
         task_id="get_rdb_one_user",
         conn_id="test",
-        sql=r"""SELECT * FROM rdb.rdb_user where {{ params.column }}='{{ params.value }}';""",
+        sql=r"""SELECT USER_ID, USER_PW, USER_NAME, USER_ROLE, IS_ADMIN FROM rdb.rdb_user where {{ params.column }}='{{ params.value }}';""",
         params={"column": "USER_ID", "value": "test12345"},
     )
     
