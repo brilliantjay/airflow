6699686f0cbd221db041e71224abb87741d08946
nayunoh <brilliantjay@naver.com>
2025-06-05 13:58:41 +0900

    modified

--- dags/dags_mariadb_test.py
+++ dags/dags_mariadb_test.py
@@ -15,7 +15,7 @@ with DAG(
         task_id="create_country_table",
         conn_id="test",
         sql=r"""
-        CREATE TABLE rdb.Country2 (
+        CREATE TABLE rdb.Country3 (
             country_id INT NOT NULL AUTO_INCREMENT,
             name VARCHAR(50),
             continent VARCHAR(50),
@@ -28,13 +28,14 @@ with DAG(
         task_id="populate_user_table",
         conn_id="test",
         sql=r"""
-                INSERT INTO rdb.rdb_user (USER_NAME, USER_ID, USER_ROLE, USER_PW,IS_ADMIN) VALUES ('test', 'test12345', 'ADMIN', 'welco123!','Y');               
+                INSERT INTO rdb.rdb_user (USER_NAME, USER_ID, USER_ROLE, USER_PW,IS_ADMIN) VALUES ('test1', 'test123456', 'ADMIN', 'welco123!','Y');               
                 """,
     )
     get_rdb_user = SQLExecuteQueryOperator(
         task_id="get_rdb.rdb_user",
         conn_id="test",
         sql=r"""SELECT * FROM rdb.rdb_user;""",
+        handler=lambda x: print(x) 
     )
    
     get_rdb_one_user = SQLExecuteQueryOperator(
