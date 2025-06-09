ebebad2f4053e464b9e7126f09cf44b3044194c8
nayunoh <brilliantjay@naver.com>
2025-06-09 17:21:28 +0900

    http

---
+++ dags/dags_python_with_http.py
@@ -0,0 +1,34 @@
+from airflow.operators.python import PythonOperator
+from airflow.providers.http.operators.http import HttpOperator
+from airflow import DAG
+from airflow.decorators import task
+
+import pendulum
+
+
+with DAG(
+    dag_id="dags_python_with_postgres_hook",
+    schedule=None,
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:    
+
+    tb_cycle_station_info = HttpOperator(
+        task_id="insrt_postgres",
+        http_conn_id = "openapi.seoul.go.kr",
+        endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/json/tpssRouteSectionTime/1/10/',
+        method="GET",
+        headers={'Content-Type':"application/json",'charset':'utf-8','Accept':'*/*'}       
+    )  
+
+    @task(task_id='python_2')
+    def python_2(**kwargs):
+        ti = kwargs['ti']
+        rslt = ti.xcom.pull(task_ids='tb_cycle_station_info')
+        import json
+        from pprint import pprint
+        pprint(json.loads(rslt))
+
+
+
+    tb_cycle_station_info >> python_2
