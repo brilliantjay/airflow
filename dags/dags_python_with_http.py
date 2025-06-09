be93fe5286f91b60f7b5dcde2100a9530e7bb236
nayunoh <brilliantjay@naver.com>
2025-06-09 17:22:42 +0900

    'change'

--- dags/dags_python_with_http.py
+++ dags/dags_python_with_http.py
@@ -7,7 +7,7 @@ import pendulum
 
 
 with DAG(
-    dag_id="dags_python_with_postgres_hook",
+    dag_id="dags_python_with_http",
     schedule=None,
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False,
