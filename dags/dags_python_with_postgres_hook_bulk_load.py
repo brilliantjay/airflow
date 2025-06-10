0340b286fd96dd30525585bec43e41259e4f7cfe
nayunoh <brilliantjay@naver.com>
2025-06-10 15:21:14 +0900

    commit

--- dags/dags_python_with_postgres_hook_bulk_load.py
+++ dags/dags_python_with_postgres_hook_bulk_load.py
@@ -13,13 +13,13 @@ with DAG(
 ) as dag:
     def insert_postgres(postgres_conn_id, tbl_nm, file_nm, **kwargs):
         custom_postgres_hook = CustomPostgresHook(postgres_conn_id = postgres_conn_id)
-        custom_postgres_hook.bulk_load(table_name=tbl_nm,file_name=file_nm, delimiter=',', is_header=True, is_replace=True)
+        custom_postgres_hook.bulk_load(table_name=tbl_nm,file_name=file_nm, delimiter='\t', is_header=True, is_replace=True)
 
 
     insrt_postgres = PythonOperator(
         task_id='insrt_postgres',
         python_callable=insert_postgres,
-        op_kwargs={'postgres_conn_id':'conn-db-postgres-custom',  'tbl_nm':"tvCorona19VaccinestatNew_bulk2",
+        op_kwargs={'postgres_conn_id':'conn-db-postgres-custom',  'tbl_nm':"tvCorona19VaccinestatNew_bulk3",
                    'file_nm':'/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}/tvCorona19VaccinestatNew.csv'})            
       
 
